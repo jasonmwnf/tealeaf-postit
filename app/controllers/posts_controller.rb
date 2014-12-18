bf6af0a1223fa88ed046359b57efaeb73a43dcd5
jasonmwnf <jasonmw_nf@hotmail.com>
2014-12-18 13:46:01 -0330

    completed functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
+  before_action :require_user, except: [:show, :index]
 
   def index
-    @posts = Post.all 
+    @posts = Post.all.sort_by{|x| x.total_votes}.reverse
   end
 
-  def show; end
+  def show
+    @comment = Comment.new
+  end
 
   def new
     @post = Post.new
@@ -13,7 +16,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user = User.first
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Your post was created."
@@ -34,9 +37,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def vote 
+    vote = Vote.create(voteable: @post, user: current_user, vote: params[:vote])
+
+    if vote.valid?
+      flash[:notice] = "You have successfully voted."
+    else
+      flash[:error] = "You can only vote on a post once."
+    end
+
+    redirect_to :back
+  end
+
   private
     def post_params
-      params.require(:post).permit!
+      params.require(:post).permit(:title, :url, :description, category_ids: [])
     end
 
   def set_post
